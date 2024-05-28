6eaf5c356af01f5e50d7937bf36c531a0726681c
SoraYuuEU <azdiva3531@gmail.com>
2024-05-28 11:04:25 +0700

    Update autoplaybase2.lua

--- autoplaybase2.lua
+++ autoplaybase2.lua
@@ -14,11 +14,7 @@ getgenv().autoPlay = {
 
 getgenv().clockGift = { -- randomly selected from list
     users = {
-        "HYM2ySHa",
-		"bkZpPT1M",
-		"kVLJiBbW",
-		"p3tg9go4",
-		"cGAnOI1S",
+        --"HYM2ySHa",
 
     }
 }
@@ -914,7 +910,7 @@ if game.PlaceId == 13775256536 then
 
     G2L["4"]["Text"] = [[Status: Joining map: ]] .. getgenv().autoPlay.map;
     print("Lobby | Joining map: " .. getgenv().autoPlay.map)
-    if readfile(game.Players.LocalPlayer.Name .. "level.txt") ~= "80" then
+    if readfile(game.Players.LocalPlayer.Name .. "level.txt") ~= "40" then
         startGame(getgenv().autoPlay.map, 1)
     end
 elseif game.PlaceId == 14082129854 then
