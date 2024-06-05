0a74df70a4f82c53ee8e81ecc95b44fe4bac0322
SoraYuuEU <azdiva3531@gmail.com>
2024-06-05 10:00:11 +0700

    Update anime.lua

--- anime.lua
+++ anime.lua
@@ -1,4 +1,10 @@
-local notifyWebhook = "https://discord.com/api/webhooks/1245501091139948655/HmEXTMvqt-y-_HMtfKp3t7tiK9p2dMxNOnKgUDzLeB12KnNE9qyvqFYs4qYAi6cPI3t0"
+local webhooks = {
+    ["Rare"] = "",
+	["Epic"] = "https://discord.com/api/webhooks/1247745918132817940/3iz-dKpd8hcJWS6SDqtBZjzADbf80QPnrCaeVM9ZL17ZiQiL2BP6hXeTaYKCaFyk5yus",
+    ["Legendary"] = "https://discord.com/api/webhooks/1247746014849269811/rJoQkIFhQowY0nTh-S7Lfxte-oBibj5YSrW_k6CyHgSgO9f6jpWG90hqI56EPJ0xlWLq",
+    ["Mythic"] = "https://discord.com/api/webhooks/1247746549921087509/03K6KobirF9kHjohQrHbZjYsWCHJ3VvEi_0kUfy-W4KJ6cPfT_cLoIjUXUVuZFhVmQ4w",
+    ["Secret"] = "https://discord.com/api/webhooks/1247746617780731914/8ue96sjIFmHFnan0Hh2bIW_J-TQvP5MXLR5y_RrJ14Y43_LiFke9VgnCofsY-aukjHoP",
+}
 
 local notifyIfRarity = {
     ["Rare"] = false,
@@ -32,7 +38,7 @@ task.wait(5)
 
 -- redeem code
 --for i, v in codes do
-   -- game:GetService("ReplicatedStorage").Remotes.UseCode:InvokeServer(v)
+    --game:GetService("ReplicatedStorage").Remotes.UseCode:InvokeServer(v)
     --task.wait(0.1)
 --end
 
@@ -90,7 +96,7 @@ for i, v in save().Units do
     -- send webhook if got troop
     if notifyIfRarity[rarity] then
         webhook(
-            notifyWebhook,
+            webhooks[rarity],
             {
                 ["content"] = game.Players.LocalPlayer.Name .. " rolled a rare troop",
                 ["username"] = "Anime defender",
