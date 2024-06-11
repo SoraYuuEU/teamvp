4bc64ffe742e0ef6c6d94911a066ad96482c0b40
SoraYuuEU <azdiva3531@gmail.com>
2024-06-11 15:14:12 +0700

    Update anime.lua

--- anime.lua
+++ anime.lua
@@ -1,50 +1,46 @@
-local notifyWebhook = "https://discord.com/api/webhooks/1249992250377896036/AhPQWiSsBOQ7sqWUX9f9nPoiVJHNt5ZMwmarqKCm0JfRhOiaroF4qV6Qlm0z6S8_cFsS"
-local type = "Update1" -- Update1, Standard
+local webhooks = {
+    ["Rare"] = "",
+	["Epic"] = "https://discord.com/api/webhooks/1247745918132817940/3iz-dKpd8hcJWS6SDqtBZjzADbf80QPnrCaeVM9ZL17ZiQiL2BP6hXeTaYKCaFyk5yus",
+    ["Legendary"] = "https://discord.com/api/webhooks/1247746014849269811/rJoQkIFhQowY0nTh-S7Lfxte-oBibj5YSrW_k6CyHgSgO9f6jpWG90hqI56EPJ0xlWLq",
+    ["Mythic"] = "https://discord.com/api/webhooks/1247746549921087509/03K6KobirF9kHjohQrHbZjYsWCHJ3VvEi_0kUfy-W4KJ6cPfT_cLoIjUXUVuZFhVmQ4w",
+    ["Secret"] = "https://discord.com/api/webhooks/1247746617780731914/8ue96sjIFmHFnan0Hh2bIW_J-TQvP5MXLR5y_RrJ14Y43_LiFke9VgnCofsY-aukjHoP",
+}
 
---stops if it got it
 local notifyIfRarity = {
     ["Rare"] = false,
+	["Epic"] = true,
     ["Legendary"] = true,
     ["Mythic"] = true,
     ["Secret"] = true,
 }
 
-local stopIfRarity = {
-    ["Rare"] = false,
-    ["Legendary"] = true,
-    ["Mythic"] = true,
-    ["Secret"] = true,
-}
-
-redeemCodes = false
 local codes = {
-    'subcool',
-    'sub2toadboigaming',
-    'sub2mozking',
-    'sub2karizmaqt',
-    'sub2jonaslyz',
-    'sub2riktime',
-    'sub2nagblox',
-    'release2024',
+'raidsarecool',
+'sorry4delay',
+'thanks400k',
+'dayum100m',
+'wsindach4ht',
+'200kholymoly',
+'adontop',
+'subcool',
+'sub2toadboigaming',
+'sub2mozking',
+'sub2karizmaqt',
+'sub2jonaslyz',
+'sub2riktime',
+'sub2nagblox',
 }
 
-local RS = game:GetService("ReplicatedStorage")
-local plrControl = require(game.ReplicatedFirst.Classes.PlayerControl)
+-- wait for game load
+repeat task.wait(5) until game:IsLoaded()
 
-if plrControl.LocalPlayer == nil then
-    plrControl.LocalPlayerLoaded.Event:Wait()
-end
-
-repeat task.wait(0.1) until game:IsLoaded()
+task.wait(5)
 
 -- redeem code
-for i, v in codes do
-    if not redeemCodes then continue end
-    pcall(function()
-    game:GetService("ReplicatedStorage").Remotes.UseCode:InvokeServer(v)
-    end)
-    task.wait(0.1)
-end
+--for i, v in codes do
+    --game:GetService("ReplicatedStorage").Remotes.UseCode:InvokeServer(v)
+    --task.wait(0.1)
+--end
 
 -- get gem using UI 
 local gems = game:GetService("Players").LocalPlayer.PlayerGui.HUD.Toolbar.CurrencyList.Gems.TextLabel
@@ -54,6 +50,10 @@ function getGems()
 end
 
 -- summon till you cant afford
+repeat
+game:GetService("ReplicatedStorage").Remotes.Summon:InvokeServer("Update1", 1)
+task.wait(1)
+until getGems() < 50
 
 function webhook(url, data)
     pcall(function()
@@ -72,6 +72,7 @@ end
 
 local rarityConvert = {
     [2] = "Rare",
+	[3] = "Epic",
     [4] = "Legendary",
     [5] = "Mythic",
     [6] = "Secret"
@@ -83,35 +84,11 @@ end
 
 -- check inventory for troops of rarity
 local UnitData = require(game:GetService("ReplicatedStorage").Modules.Bins.UnitData)
-local UnitDatav3 = require(RS:WaitForChild("Actions"))
 local unitDataV2 = {}
 
 for i, v in UnitData do
     unitDataV2[i] = v.Rarity
 end
-
-for i, v in pairs(UnitDatav3) do
-    if i == "UNIT_EQUIP_BEST" then
-        UnitDatav3 = v.name
-    end
-end
-
-local done = false
-repeat
-game:GetService("ReplicatedStorage").Remotes.Summon:InvokeServer(type, 1)
-task.wait(1)
-
-for i, v in save().Units do
-    local rarity = rarityConvert[unitDataV2[v.Type]]
-
-    -- send webhook if got troop
-    if stopIfRarity[rarity] then
-       done = true
-    end
-end
-
-until getGems() < 50 or done
-
 
 for i, v in save().Units do
     local rarity = rarityConvert[unitDataV2[v.Type]]
@@ -119,14 +96,14 @@ for i, v in save().Units do
     -- send webhook if got troop
     if notifyIfRarity[rarity] then
         webhook(
-            notifyWebhook,
+            webhooks[rarity],
             {
                 ["content"] = game.Players.LocalPlayer.Name .. " rolled a rare troop",
                 ["username"] = "Anime defender",
                 ["embeds"] = {
                     {
                         ["title"] = game.Players.LocalPlayer.Name ..
-                        " got a: " .. notifyIfRarity[rarity] .. " " .. v.Type,
+                        " got a: " .. rarity .. " " .. v.Type,
                         ["type"] = "rich",
                         ["color"] = 8388736,
                         ["author"] = {
@@ -138,3 +115,7 @@ for i, v in save().Units do
             })
     end
 end
+
+
+--write for yummytool
+writefile(game.Players.LocalPlayer.Name .. ".txt","Yummytool")
